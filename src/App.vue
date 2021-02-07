1d5102d3526b46683b1a19cf466af368d5570002
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-02-07 21:04:04 +0700

    Create BottomNavigation.vue

--- src/App.vue
+++ src/App.vue
@@ -8,39 +8,18 @@
         bottom: 0;
         min-width: 100%;
     ">
-      <v-bottom-navigation v-model="value">
-        <v-btn value="recent">
-          <span>Recent</span>
-
-          <v-icon>mdi-history</v-icon>
-        </v-btn>
-
-        <v-btn value="favorites">
-          <span>Favorites</span>
-
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-
-        <v-btn value="nearby">
-          <span>Nearby</span>
-
-          <v-icon>mdi-map-marker</v-icon>
-        </v-btn>
-      </v-bottom-navigation>
+      <BottomNavigation :value="value"/>
     </div>
   </div>
 </template>
 
 <script>
 
+import BottomNavigation from "@/BottomNavigation";
+
 export default {
   name: 'App',
-  components: {},
-  data: () => (
-      {
-        value: 'recent',
-      }
-  ),
+  components: {BottomNavigation},
 }
 </script>
 
---
+++ src/BottomNavigation.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-bottom-navigation v-model="value">
+    <v-btn value="consultants">
+      <span>Konsultan</span>
+
+      <v-icon>mdi-account-group-outline</v-icon>
+    </v-btn>
+    <v-btn value="schedules">
+      <span>Jadwal</span>
+
+      <v-icon>mdi-calendar</v-icon>
+    </v-btn>
+    <v-btn value="home">
+      <span>Home</span>
+
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+    <v-btn value="cart">
+      <span>Transaksi</span>
+
+      <v-icon>mdi-cart</v-icon>
+    </v-btn>
+    <v-btn value="profile">
+      <span>Profil</span>
+
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+<script>
+export default {
+  name: 'BottomNavigation',
+  props: {
+    value: {}
+  },
+  data: () => (
+      {
+        value: 'home',
+      }
+  ),
+}
+</script>
+<style>
+span {
+  font-size: 80%;
+}
+</style>
