6b6c0182f64b9b600e7b02d90d00d47cd6f3d97e
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-02-07 21:32:27 +0700

    Add todo.js

---
+++ todo.js
@@ -0,0 +1,21 @@
+/*
+* - implement routing
+* - implement PWA
+*
+* - implement consultants page
+* - implement schedules page
+* - implement profile page
+*
+* - implement vuex
+*
+* - load assets
+*
+* - attach datepicker
+* - attach timepicker
+*
+* - connect to evaluatte api
+*
+* - adjust styling
+*   - adjust color
+*   - adjust theming
+* */
