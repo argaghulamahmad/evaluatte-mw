a97df6a6c2005364ff6850c8ed79676915b8a138
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-02-13 15:40:16 +0700

    Create Order.vue

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -1,23 +1,90 @@
 <template>
   <div>
-    <v-date-picker
-        v-model="date"
-        :allowed-dates="allowedDates"
-        min="2016-06-15"
-        max="2018-03-20"
-    ></v-date-picker>
+    <v-card class="overflow-hidden">
+      <v-app-bar
+          fixed
+          color="#212529"
+          dark
+      >
+        <v-app-bar-nav-icon>
+          <v-icon @click="
+            hasHistory()
+            ? $router.go(-1)
+            : $router.push('/')
+          "
+          >mdi-chevron-left</v-icon>
+        </v-app-bar-nav-icon>
+
+        <v-toolbar-title>Konsultasi</v-toolbar-title>
+      </v-app-bar>
+      <div
+          class="overflow-y-auto"
+      >
+        <div style="margin-top: 20%;"/>
+
+        <div class="mx-2 elevation-2">
+          <h3 class="ml-2 mt-2 pl-2 pt-2 pb-2">Atur Tanggal</h3>
+          <v-date-picker
+              v-model="date"
+              :allowed-dates="allowedDates"
+              min="2016-06-15"
+              max="2018-03-20"
+
+              full-width
+          ></v-date-picker>
+        </div>
+
+        <div class="mx-2 elevation-2">
+          <h3 class="ml-2 mt-2 pl-2 pt-2 pb-2">Atur Jam</h3>
+          <v-time-picker
+              v-model="time"
+              :allowed-hours="allowedHours"
+              :allowed-minutes="allowedMinutes"
+              class="mt-4"
+              format="24hr"
+              scrollable
+              min="9:30"
+              max="22:15"
+
+              full-width
+          ></v-time-picker>
+        </div>
+
+        <v-row class="mt-4 mx-1">
+          <v-col cols="6">
+          </v-col>
+          <v-col cols="6">
+            <v-btn
+                outlined
+                text
+                style="
+                        min-width: 100%;
+                        background: #FCCF14;
+                        color: #FFFFFF;
+                        border: none;
+                      "
+                to="/order"
+            >
+              Lanjut Daftar
+            </v-btn>
+          </v-col>
+        </v-row>
+
+        <div style="margin-top: 20%;"/>
+      </div>
+    </v-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: "MakeSchedule",
+  name: "Order",
   data: () => ({
     date: '2018-03-02',
   }),
-
   methods: {
     allowedDates: val => parseInt(val.split('-')[2], 10) % 2 === 0,
+    hasHistory: () => window.history.length > 2
   },
 }
 </script>
